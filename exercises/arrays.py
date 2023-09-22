d43a88a023a657304a6720a9c0b2176c8eeaf6de
iamDenisLesiv <iam@denislesiv.ru>
2023-09-22 20:32:48 +0300

    aray_done

--- exercises/arrays.py
+++ exercises/arrays.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 mystery = np.array([[-13586,  15203,  28445, -27117,  -1781, -17182, -18049],
        [ 25936, -30968,  -1297,  -4593,   6451,  15790,   7181],
        [ 13348,  28049,  28655,  -6012,  21762,  25397,   8225],
@@ -18,9 +19,20 @@ col_2 = mystery[:, -2]
 print(col_2)
 part = mystery[1:4:, 2:5]
 print(part)
-rev0 = mystery[:,-1:]
-rev = rev0[::-1]
-print(rev0)
-print(rev)
+#rev0 = mystery[0:,-1:]
+#rev1 = rev0[::-1]
+#rev2 = rev1.transpose()
+#rev = rev2.tolist()
+#rev = []
+#for item in rev2:
+#    rev.append(item)
+#print(rev0)
+#print(rev1)
+#print(rev2)
+#print(rev)
 trans = mystery.transpose()
 print(trans)
+rev1 = trans[-1:][::-1,::-1]
+rev = rev1[0]
+print(rev)
+print(rev1)

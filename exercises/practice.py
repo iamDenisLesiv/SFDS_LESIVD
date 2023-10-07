0a3d012a5cde32e6de77874d0de1c5715f3faf21
iamDenisLesiv <iam@denislesiv.ru>
2023-10-07 12:26:37 +0300

    33

--- exercises/practice.py
+++ exercises/practice.py
@@ -1,18 +1,3 @@
-from collections import defaultdict, deque
-
-def task_manager(tasks):
-    task_dict = defaultdict(deque)
-    for task in tasks:
-        if task[2]:
-            task_dict[task[1]].appendleft(task[0])
-        else:
-            task_dict[task[1]].append(task[0])
-    return task_dict
-
-tasks = [(36871, 'office', False),
-(40690, 'office', False),
-(35364, 'voltage', False),
-(41667, 'voltage', True),
-(33850, 'office', False)]
-
-print(task_manager(tasks))
+import pandas as pd
+print(pd. __version__)
+print(pd. __name__)
